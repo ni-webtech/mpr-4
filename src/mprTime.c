2a554800e0751ecb65be6411b3e946289e866b31
Embedthis Software <dev@embedthis.com>
2010-04-16 16:33:42 -0700

    Fix windows time zone

--- src/mprTime.c
+++ src/mprTime.c
@@ -1198,8 +1198,10 @@ static int gettimeofday(struct timeval *tv, struct timezone *tz)
         } else {
             tz->tz_dsttime = 0;
         }
+#if UNUSED
         bias *= 60;
         /* UTC = local + bias */
+#endif
         tz->tz_minuteswest = bias;
     }
 
