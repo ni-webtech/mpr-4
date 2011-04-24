c3c43899a94ce039118bb37aaea83bdde29702b9
Embedthis Software <dev@embedthis.com>
2011-04-23 23:14:31 -0700

    Add null to Cmd buffers

--- src/mprCmd.c
+++ src/mprCmd.c
@@ -370,11 +370,9 @@ int mprRunCmdV(MprCmd *cmd, int argc, char **argv, char **out, char **err, int f
         return MPR_ERR;
     }
     if (err && flags & MPR_CMD_ERR) {
-        mprAddNullToBuf(cmd->stderrBuf);
         *err = mprGetBufStart(cmd->stderrBuf);
     }
     if (out && flags & MPR_CMD_OUT) {
-        mprAddNullToBuf(cmd->stdoutBuf);
         *out = mprGetBufStart(cmd->stdoutBuf);
     }
     return status;
@@ -784,6 +782,7 @@ static void cmdCallback(MprCmd *cmd, int channel, void *data)
     } else {
         mprAdjustBufEnd(buf, len);
     }
+    mprAddNullToBuf(buf);
     mprEnableCmdEvents(cmd, channel);
 }
 
