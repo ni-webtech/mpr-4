438bb2624b8c4b4c71e0c9467fb779cc39d4ee1e
Embedthis Software <dev@embedthis.com>
2011-10-13 22:42:41 -0700

    Fix waiting for SSL with buffered data.

--- src/mprWait.c
+++ src/mprWait.c
@@ -195,6 +195,9 @@ void mprWaitOn(MprWaitHandler *wp, int mask)
 {
     lock(wp->service);
     if (mask != wp->desiredMask) {
+        if (wp->flags & MPR_WAIT_RECALL_HANDLER) {
+            wp->service->needRecall = 1;
+        }
         mprNotifyOn(wp->service, wp, mask);
         mprWakeNotifier();
     }
