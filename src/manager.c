d20efcf11408c98e1c6e3b05e3a8721583182f5b
Embedthis Software <dev@embedthis.com>
2011-11-17 20:23:57 -0800

    Debug windows manager

--- src/manager.c
+++ src/manager.c
@@ -22,10 +22,12 @@
 #if 0
 #define SERVICE_PROGRAM BLD_BIN_PREFIX "/" BLD_PRODUCT
 #define SERVICE_NAME BLD_PRODUCT
+#define SERVICE_HOME BLD_BIN_PREFIX
 
 #else
 #define SERVICE_PROGRAM "C:/cygwin/home/mob/appweb/out/bin/appweb"
 #define SERVICE_NAME "appweb"
+#define SERVICE_HOME "C:/cygwin/home/mob/appweb/src/server"
 #endif
 
 #if BLD_UNIX_LIKE
@@ -64,7 +66,7 @@ static void manageApp(void *unused, int flags);
 static int  readPid();
 static bool  process(cchar *operation, bool quiet);
 static void runService();
-static void setAppDefaults(Mpr *mpr);
+static void setAppDefaults();
 static void terminating(int how, int status);
 static int  writePid(int pid);
 
@@ -82,7 +84,7 @@ int main(int argc, char *argv[])
     mprAddRoot(app);
     mprAddTerminator(terminating);
     mprAddStandardSignals();
-    setAppDefaults(mpr);
+    setAppDefaults();
 
     for (nextArg = 1; nextArg < argc && !err; nextArg++) {
         argp = argv[nextArg];
@@ -230,7 +232,7 @@ static void manageApp(void *ptr, int flags)
 }
 
 
-static void setAppDefaults(Mpr *mpr)
+static void setAppDefaults()
 {
     app->appName = mprGetAppName();
     app->serviceProgram = sclone(SERVICE_PROGRAM);
@@ -750,22 +752,22 @@ static SERVICE_TABLE_ENTRY      svcTable[] = {
 };
 
 static void     WINAPI serviceCallback(ulong code);
-static int      enableService(int enable);
-static void     initService();
-static int      installService();
+static bool     enableService(int enable);
+static void     setAppDefaults();
+static bool     installService();
 static void     logHandler(int flags, int level, cchar *msg);
 static int      registerService();
 static int      removeService(int removeFromScmDb);
 static void     gracefulShutdown(MprTime timeout);
 static bool     process(cchar *operation);
+static void     run();
 static int      startDispatcher(LPSERVICE_MAIN_FUNCTION svcMain);
-static int      startService();
-static int      stopService(int cmd);
+static bool     startService();
+static bool     stopService(int cmd);
 static int      tellSCM(long state, long exitCode, long wait);
 static void     terminating(int how, int status);
 static void     updateStatus(int status, int exitCode);
 static void     writeToOsLog(cchar *message);
-static void     run();
 
 /***************************** Forward Declarations ***************************/
 
@@ -791,7 +793,7 @@ int APIENTRY WinMain(HINSTANCE inst, HINSTANCE junk, char *args, int junk2)
     app->appInst = inst;
     app->heartBeatPeriod = HEART_BEAT_PERIOD;
 
-    initService();
+    setAppDefaults();
 
     mprSetAppName(BLD_PRODUCT "Manager", BLD_NAME "Manager", BLD_VERSION);
     app->appName = mprGetAppName();
@@ -924,7 +926,10 @@ int APIENTRY WinMain(HINSTANCE inst, HINSTANCE junk, char *args, int junk2)
         mprUserError("Can't start MPR for %s", mprGetAppName());                                           
     } else {
         mprStartEventsThread();
-        for (; nextArg < argc; nextArg++) {
+        if (nextArg >= argc) {
+            return process("run");
+
+        } else for (; nextArg < argc; nextArg++) {
             if (!process(argv[nextArg])) {
                 return FALSE;
             }
@@ -944,49 +949,40 @@ static void manageApp(void *ptr, int flags)
         mprMark(app->serviceProgram);
         mprMark(app->serviceTitle);
         mprMark(app->serviceArgs);
+        mprMark(app->logSpec);
     }
 }
 
 
 static bool process(cchar *operation)
 {
+    bool    rc;
+
+    rc = 1;
+
     if (smatch(operation, "install")) {
-        if (installService(app->serviceArgs) < 0) {
-            return 0;
-        }
+        rc = installService(app->serviceArgs);
 
     } else if (smatch(operation, "uninstall")) {
-        if (removeService(1) < 0) {
-            return 0;
-        }
+        rc = removeService(1);
 
     } else if (smatch(operation, "enable")) {
-        if (enableService(1) < 0) {
-            return 0;
-        }
+        rc = enableService(1);
 
     } else if (smatch(operation, "disable")) {
-        if (enableService(0) < 0) {
-            return 0;
-        }
+        rc = enableService(0);
 
     } else if (smatch(operation, "start")) {
-        if (startService() < 0) {
-            return 0;
-        }
+        rc = startService();
 
     } else if (smatch(operation, "stop")) {
-        if (removeService(0) < 0) {
-            return 0;
-        }
+        rc = removeService(0);
 
     } else if (smatch(operation, "reload")) {
-        return process("restart");
+        rc = process("restart");
 
     } else if (smatch(operation, "restart")) {
-        if (!process("stop")) {
-            return MPR_ERR_CANT_COMPLETE;
-        }
+        process("stop");
         return process("start");
 
     } else if (smatch(operation, "run")) {
@@ -996,7 +992,7 @@ static bool process(cchar *operation)
          */
         startDispatcher(serviceMain);
     }
-    return 1;
+    return rc;
 }
 
 
@@ -1065,7 +1061,7 @@ static void run()
 
     createFlags = 0;
 
-#if USEFUL_FOR_DEBUG
+#if USEFUL_FOR_DEBUG || 1
     DebugBreak();
 #endif
 
@@ -1168,7 +1164,7 @@ static int startDispatcher(LPSERVICE_MAIN_FUNCTION svcMain)
      */
     svcTable[0].lpServiceProc = svcMain;
     if (StartServiceCtrlDispatcher(svcTable) == 0) {
-        mprError("Could not start the service control dispatcher");
+        mprError("Could not start the service control dispatcher: 0x%x", GetLastError());
         return MPR_ERR_CANT_INITIALIZE;
     }
     return 0;
@@ -1183,7 +1179,7 @@ static int registerService()
 {
     svcHandle = RegisterServiceCtrlHandler(app->serviceName, serviceCallback);
     if (svcHandle == 0) {
-        mprError("Can't register handler: %x", GetLastError());
+        mprError("Can't register handler: 0x%x", GetLastError());
         return MPR_ERR_CANT_INITIALIZE;
     }
     /*
@@ -1242,7 +1238,7 @@ static void WINAPI serviceCallback(ulong cmd)
 }
 
 
-static int installService()
+static bool installService()
 {
     SC_HANDLE   svc, mgr;
     char        cmd[MPR_MAX_FNAME], key[MPR_MAX_FNAME];
@@ -1251,7 +1247,7 @@ static int installService()
     mgr = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
     if (! mgr) {
         mprUserError("Can't open service manager");
-        return MPR_ERR_CANT_ACCESS;
+        return 0;
     }
     /*
         Install this app as a service
@@ -1268,7 +1264,7 @@ static int installService()
         if (! svc) {
             mprUserError("Can't create service: 0x%x == %d", GetLastError(), GetLastError());
             CloseServiceHandle(mgr);
-            return MPR_ERR_CANT_CREATE;
+            return 0;
         }
     }
     CloseServiceHandle(svc);
@@ -1280,7 +1276,7 @@ static int installService()
     mprSprintf(key, sizeof(key), "HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\" "Services\\%s", app->serviceName);
 
     if (mprWriteRegistry(key, "Description", SERVICE_DESCRIPTION) < 0) {
-        return MPR_ERR_CANT_WRITE;
+        return 0;
     }
 
     /*
@@ -1290,7 +1286,7 @@ static int installService()
         app->serviceHome = mprGetPathParent(mprGetAppDir());
     }
     if (mprWriteRegistry(key, "HomeDir", app->serviceHome) < 0) {
-        return MPR_ERR_CANT_WRITE;
+        return 0;
     }
 
     /*
@@ -1298,17 +1294,17 @@ static int installService()
      */
     if (app->serviceArgs && *app->serviceArgs) {
         if (mprWriteRegistry(key, "Args", app->serviceArgs) < 0) {
-            return MPR_ERR_CANT_WRITE;
+            return 0;
         }
     }
-    return 0;
+    return 1;
 }
 
 
 /*
     Remove the application service
  */ 
-static int removeService(int removeFromScmDb)
+static bool removeService(int removeFromScmDb)
 {
     SC_HANDLE   svc, mgr;
 
@@ -1317,13 +1313,13 @@ static int removeService(int removeFromScmDb)
     mgr = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
     if (! mgr) {
         mprError("Can't open service manager");
-        return MPR_ERR_CANT_ACCESS;
+        return 0;
     }
     svc = OpenService(mgr, app->serviceName, SERVICE_ALL_ACCESS);
     if (! svc) {
         CloseServiceHandle(mgr);
         mprError("Can't open service");
-        return MPR_ERR_CANT_OPEN;
+        return 0;
     }
     gracefulShutdown(0);
 
@@ -1338,55 +1334,50 @@ static int removeService(int removeFromScmDb)
             }
         }
         if (svcStatus.dwCurrentState != SERVICE_STOPPED) {
-            mprError("Can't stop service: %x", GetLastError());
+            mprError("Can't stop service: 0x%x", GetLastError());
         }
     }
     if (removeFromScmDb && !DeleteService(svc)) {
-        mprError("Can't delete service: %x", GetLastError());
+        if (GetLastError() != ERROR_SERVICE_MARKED_FOR_DELETE) {
+            mprError("Can't delete service: 0x%x", GetLastError());
+        }
     }
     CloseServiceHandle(svc);
     CloseServiceHandle(mgr);
-    return 0;
+    return 1;
 }
 
 
-static int enableService(int enable)
+static bool enableService(int enable)
 {
     SC_HANDLE   svc, mgr;
-    int         flag, serviceType;
+    int         flag;
 
     mgr = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
     if (! mgr) {
         mprUserError("Can't open service manager");
-        return MPR_ERR_CANT_ACCESS;
+        return 0;
     }
     svc = OpenService(mgr, app->serviceName, SERVICE_ALL_ACCESS);
     if (svc == NULL) {
         mprUserError("Can't access service");
         CloseServiceHandle(mgr);
-        return MPR_ERR_CANT_ACCESS;
-    }
-    serviceType = SERVICE_WIN32_OWN_PROCESS;
-    if (app->createConsole) {
-        serviceType |= SERVICE_INTERACTIVE_PROCESS;
+        return 0;
     }
     flag = (enable) ? SERVICE_AUTO_START : SERVICE_DISABLED;
     if (!ChangeServiceConfig(svc, SERVICE_NO_CHANGE, flag, SERVICE_NO_CHANGE, NULL, NULL, NULL, NULL, NULL, NULL, NULL)) {
         mprUserError("Can't change service: 0x%x == %d", GetLastError(), GetLastError());
         CloseServiceHandle(svc);
         CloseServiceHandle(mgr);
-        return MPR_ERR_CANT_CREATE;
+        return 0;
     }
     CloseServiceHandle(svc);
     CloseServiceHandle(mgr);
-    return 0;
+    return 1;
 }
 
 
-/*
-    Start the window's service
- */ 
-static int startService()
+static bool startService()
 {
     SC_HANDLE   svc, mgr;
     int         rc;
@@ -1396,29 +1387,27 @@ static int startService()
     mgr = OpenSCManager(NULL, NULL, SC_MANAGER_ALL_ACCESS);
     if (! mgr) {
         mprError("Can't open service manager");
-        return MPR_ERR_CANT_ACCESS;
+        return 0;
     }
     svc = OpenService(mgr, app->serviceName, SERVICE_ALL_ACCESS);
     if (! svc) {
         mprError("Can't open service");
         CloseServiceHandle(mgr);
-        return MPR_ERR_CANT_OPEN;
+        return 0;
     }
     rc = StartService(svc, 0, NULL);
-    if (rc == 0) {
-        mprError("Can't start %s service: %d", app->serviceName, GetLastError());
-        return MPR_ERR_CANT_INITIALIZE;
-    }
     CloseServiceHandle(svc);
     CloseServiceHandle(mgr);
-    return 0;
+
+    if (rc == 0) {
+        mprError("Can't start %s service: 0x%x", app->serviceName, GetLastError());
+        return 0;
+    }
+    return 1;
 }
 
 
-/*
-    Stop the service in the current process. 
- */ 
-static int stopService(int cmd)
+static bool stopService(int cmd)
 {
     int     exitCode;
 
@@ -1427,7 +1416,7 @@ static int stopService(int cmd)
 
     gracefulShutdown(10 * 1000);
     if (cmd == SERVICE_CONTROL_SHUTDOWN) {
-        return 0;
+        return 1;
     }
 
     /*
@@ -1448,7 +1437,7 @@ static int stopService(int cmd)
     }
     svcStatus.dwCurrentState = SERVICE_STOPPED;
     tellSCM(svcStatus.dwCurrentState, exitCode, 0);
-    return 0;
+    return 1;
 }
 
 
@@ -1481,10 +1470,11 @@ static int tellSCM(long state, long exitCode, long wait)
 }
 
 
-static void initService()
+static void setAppDefaults()
 {
-    app->serviceName = BLD_COMPANY "-" BLD_PRODUCT;
-    app->serviceTitle = BLD_NAME;
+    app->serviceName = sclone(BLD_COMPANY "-" BLD_PRODUCT);
+    app->serviceHome = mprGetNativePath(SERVICE_HOME);
+    app->serviceTitle = sclone(BLD_NAME);
     app->serviceStopped = 0;
     app->serviceProgram = sjoin(mprGetAppDir(), "/", BLD_PRODUCT, ".exe", NULL);
 }
