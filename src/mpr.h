127a6dd8f7cec1c408e6e073c03f4529d85542ee
Embedthis Software <dev@embedthis.com>
2011-12-16 14:47:24 -0800

    Comp fixes for VxWorks

--- src/mpr.h
+++ src/mpr.h
@@ -429,6 +429,15 @@
  */
 typedef int64 MprOff;
 
+/*
+    Socklen_t
+ */
+#if VXWORKS
+    typedef int MprSocklen;
+#else
+    typedef socklen_t MprSocklen;
+#endif
+
 /**
     Date and Time Service
     @stability Evolving
