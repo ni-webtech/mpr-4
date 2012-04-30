eff02068cba0118d3a09c415624cfc75d5fe3200
Embedthis Software <dev@embedthis.com>
2012-04-30 15:46:56 -0700

    Regen projects

--- projects/mpr-win.sh
+++ projects/mpr-win.sh
@@ -2,12 +2,6 @@
 #   mpr-win.sh -- Build It Shell Script to build Multithreaded Portable Runtime
 #
 
-VS="${VSINSTALLDIR}"
-: ${VS:="\Users\mob\git\mpr\$(VS)"}
-SDK="${WindowsSDKDir}"
-: ${SDK:="$(SDK)"}
-
-export SDK VS
 export PATH="$(SDK)/Bin:$(VS)/VC/Bin:$(VS)/Common7/IDE:$(VS)/Common7/Tools:$(VS)/SDK/v3.5/bin:$(VS)/VC/VCPackages;$(PATH)"
 export INCLUDE="$(INCLUDE);$(SDK)/INCLUDE:$(VS)/VC/INCLUDE"
 export LIB="$(LIB);$(SDK)/lib:$(VS)/VC/lib"
