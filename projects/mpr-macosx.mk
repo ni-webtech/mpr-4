e6ad7f2abc4b6f1b72b7e7f60cd16391bc562f84
Embedthis Software <dev@embedthis.com>
2012-05-07 09:13:07 -0700

    Fix nmake scripts

--- projects/mpr-macosx.mk
+++ projects/mpr-macosx.mk
@@ -6,7 +6,7 @@ ARCH     := $(shell uname -m | sed 's/i.86/x86/')
 OS       := macosx
 PROFILE  := debug
 CONFIG   := $(OS)-$(ARCH)-$(PROFILE)
-CC       := /usr/bin/gcc
+CC       := /usr/bin/clang
 LD       := /usr/bin/ld
 CFLAGS   := -Wno-deprecated-declarations -g -w
 DFLAGS   := -DBLD_DEBUG
@@ -123,222 +123,266 @@ $(CONFIG)/inc/mpr.h:
 
 $(CONFIG)/obj/dtoa.o: \
         src/dtoa.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/dtoa.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/dtoa.c
 
 $(CONFIG)/obj/mpr.o: \
         src/mpr.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mpr.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mpr.c
 
 $(CONFIG)/obj/mprAsync.o: \
         src/mprAsync.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprAsync.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprAsync.c
 
 $(CONFIG)/obj/mprAtomic.o: \
         src/mprAtomic.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprAtomic.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprAtomic.c
 
 $(CONFIG)/obj/mprBuf.o: \
         src/mprBuf.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprBuf.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprBuf.c
 
 $(CONFIG)/obj/mprCache.o: \
         src/mprCache.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprCache.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprCache.c
 
 $(CONFIG)/obj/mprCmd.o: \
         src/mprCmd.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprCmd.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprCmd.c
 
 $(CONFIG)/obj/mprCond.o: \
         src/mprCond.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprCond.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprCond.c
 
 $(CONFIG)/obj/mprCrypt.o: \
         src/mprCrypt.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprCrypt.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprCrypt.c
 
 $(CONFIG)/obj/mprDisk.o: \
         src/mprDisk.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprDisk.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprDisk.c
 
 $(CONFIG)/obj/mprDispatcher.o: \
         src/mprDispatcher.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprDispatcher.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprDispatcher.c
 
 $(CONFIG)/obj/mprEncode.o: \
         src/mprEncode.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprEncode.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprEncode.c
 
 $(CONFIG)/obj/mprEpoll.o: \
         src/mprEpoll.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprEpoll.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprEpoll.c
 
 $(CONFIG)/obj/mprEvent.o: \
         src/mprEvent.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprEvent.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprEvent.c
 
 $(CONFIG)/obj/mprFile.o: \
         src/mprFile.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprFile.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprFile.c
 
 $(CONFIG)/obj/mprFileSystem.o: \
         src/mprFileSystem.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprFileSystem.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprFileSystem.c
 
 $(CONFIG)/obj/mprHash.o: \
         src/mprHash.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprHash.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprHash.c
 
 $(CONFIG)/obj/mprJSON.o: \
         src/mprJSON.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprJSON.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprJSON.c
 
 $(CONFIG)/obj/mprKqueue.o: \
         src/mprKqueue.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprKqueue.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprKqueue.c
 
 $(CONFIG)/obj/mprList.o: \
         src/mprList.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprList.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprList.c
 
 $(CONFIG)/obj/mprLock.o: \
         src/mprLock.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprLock.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprLock.c
 
 $(CONFIG)/obj/mprLog.o: \
         src/mprLog.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprLog.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprLog.c
 
 $(CONFIG)/obj/mprMem.o: \
         src/mprMem.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprMem.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprMem.c
 
 $(CONFIG)/obj/mprMime.o: \
         src/mprMime.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprMime.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprMime.c
 
 $(CONFIG)/obj/mprMixed.o: \
         src/mprMixed.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprMixed.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprMixed.c
 
 $(CONFIG)/obj/mprModule.o: \
         src/mprModule.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprModule.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprModule.c
 
 $(CONFIG)/obj/mprPath.o: \
         src/mprPath.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprPath.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprPath.c
 
 $(CONFIG)/obj/mprPoll.o: \
         src/mprPoll.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprPoll.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprPoll.c
 
 $(CONFIG)/obj/mprPrintf.o: \
         src/mprPrintf.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprPrintf.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprPrintf.c
 
 $(CONFIG)/obj/mprRomFile.o: \
         src/mprRomFile.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprRomFile.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprRomFile.c
 
 $(CONFIG)/obj/mprSelect.o: \
         src/mprSelect.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprSelect.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprSelect.c
 
 $(CONFIG)/obj/mprSignal.o: \
         src/mprSignal.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprSignal.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprSignal.c
 
 $(CONFIG)/obj/mprSocket.o: \
         src/mprSocket.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprSocket.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprSocket.c
 
 $(CONFIG)/obj/mprString.o: \
         src/mprString.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprString.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprString.c
 
 $(CONFIG)/obj/mprTest.o: \
         src/mprTest.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprTest.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprTest.c
 
 $(CONFIG)/obj/mprThread.o: \
         src/mprThread.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprThread.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprThread.c
 
 $(CONFIG)/obj/mprTime.o: \
         src/mprTime.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprTime.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprTime.c
 
 $(CONFIG)/obj/mprUnix.o: \
         src/mprUnix.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprUnix.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprUnix.c
 
 $(CONFIG)/obj/mprVxworks.o: \
         src/mprVxworks.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprVxworks.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprVxworks.c
 
 $(CONFIG)/obj/mprWait.o: \
         src/mprWait.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprWait.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprWait.c
 
 $(CONFIG)/obj/mprWide.o: \
         src/mprWide.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprWide.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprWide.c
 
 $(CONFIG)/obj/mprWin.o: \
         src/mprWin.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprWin.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprWin.c
 
 $(CONFIG)/obj/mprWince.o: \
         src/mprWince.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprWince.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprWince.c
 
 $(CONFIG)/obj/mprXml.o: \
         src/mprXml.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprXml.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprXml.c
 
 $(CONFIG)/bin/libmpr.dylib:  \
@@ -391,7 +435,8 @@ $(CONFIG)/bin/libmpr.dylib:  \
 
 $(CONFIG)/obj/benchMpr.o: \
         test/benchMpr.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/benchMpr.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/benchMpr.c
 
 $(CONFIG)/bin/benchMpr:  \
@@ -410,17 +455,20 @@ $(CONFIG)/bin/runProgram:  \
 
 $(CONFIG)/obj/mprMatrixssl.o: \
         src/mprMatrixssl.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprMatrixssl.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprMatrixssl.c
 
 $(CONFIG)/obj/mprOpenssl.o: \
         src/mprOpenssl.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprOpenssl.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprOpenssl.c
 
 $(CONFIG)/obj/mprSsl.o: \
         src/mprSsl.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/mprSsl.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/mprSsl.c
 
 $(CONFIG)/bin/libmprssl.dylib:  \
@@ -432,87 +480,104 @@ $(CONFIG)/bin/libmprssl.dylib:  \
 
 $(CONFIG)/obj/testArgv.o: \
         test/testArgv.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testArgv.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testArgv.c
 
 $(CONFIG)/obj/testBuf.o: \
         test/testBuf.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testBuf.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testBuf.c
 
 $(CONFIG)/obj/testCmd.o: \
         test/testCmd.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testCmd.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testCmd.c
 
 $(CONFIG)/obj/testCond.o: \
         test/testCond.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testCond.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testCond.c
 
 $(CONFIG)/obj/testEvent.o: \
         test/testEvent.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testEvent.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testEvent.c
 
 $(CONFIG)/obj/testFile.o: \
         test/testFile.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testFile.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testFile.c
 
 $(CONFIG)/obj/testHash.o: \
         test/testHash.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testHash.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testHash.c
 
 $(CONFIG)/obj/testList.o: \
         test/testList.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testList.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testList.c
 
 $(CONFIG)/obj/testLock.o: \
         test/testLock.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testLock.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testLock.c
 
 $(CONFIG)/obj/testMem.o: \
         test/testMem.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testMem.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testMem.c
 
 $(CONFIG)/obj/testMpr.o: \
         test/testMpr.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testMpr.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testMpr.c
 
 $(CONFIG)/obj/testPath.o: \
         test/testPath.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testPath.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testPath.c
 
 $(CONFIG)/obj/testSocket.o: \
         test/testSocket.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testSocket.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testSocket.c
 
 $(CONFIG)/obj/testSprintf.o: \
         test/testSprintf.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testSprintf.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testSprintf.c
 
 $(CONFIG)/obj/testThread.o: \
         test/testThread.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testThread.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testThread.c
 
 $(CONFIG)/obj/testTime.o: \
         test/testTime.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testTime.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testTime.c
 
 $(CONFIG)/obj/testUnicode.o: \
         test/testUnicode.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/testUnicode.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc test/testUnicode.c
 
 $(CONFIG)/bin/testMpr:  \
@@ -540,7 +605,8 @@ $(CONFIG)/bin/testMpr:  \
 
 $(CONFIG)/obj/manager.o: \
         src/manager.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/manager.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/manager.c
 
 $(CONFIG)/bin/manager:  \
@@ -550,7 +616,8 @@ $(CONFIG)/bin/manager:  \
 
 $(CONFIG)/obj/makerom.o: \
         src/utils/makerom.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/makerom.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/utils/makerom.c
 
 $(CONFIG)/bin/makerom:  \
@@ -560,7 +627,8 @@ $(CONFIG)/bin/makerom:  \
 
 $(CONFIG)/obj/charGen.o: \
         src/utils/charGen.c \
-        $(CONFIG)/inc/bit.h
+        $(CONFIG)/inc/bit.h \
+        $(CONFIG)/inc/mpr.h
 	$(CC) -c -o $(CONFIG)/obj/charGen.o -arch x86_64 $(CFLAGS) $(DFLAGS) -I$(CONFIG)/inc src/utils/charGen.c
 
 $(CONFIG)/bin/chargen:  \
