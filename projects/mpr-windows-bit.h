e9040906ef0bc02d77a24323ae07cfb3261852ee
Embedthis Software <dev@embedthis.com>
2012-07-10 09:22:39 -0700

    Fix own-projects target

--- projects/mpr-windows-bit.h
+++ projects/mpr-windows-bit.h
@@ -16,17 +16,17 @@
 #define BIT_MINOR_VERSION 0
 #define BIT_PATCH_VERSION 1
 #define BIT_VNUM 400000001
-#define BIT_CONFIG_CMD "bit -d -q -platform windows-x86 -without all -profile vs -configure . -gen vs"
-#define BIT_PROFILE "vs"
-#define BIT_CFG_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime"
-#define BIT_BIN_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime/bin"
-#define BIT_INC_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime/inc"
-#define BIT_LOG_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime/logs"
-#define BIT_PRD_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime"
-#define BIT_SPL_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime/tmp"
-#define BIT_SRC_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime/src"
-#define BIT_VER_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime"
-#define BIT_WEB_PREFIX "C:/Program Files (x86)/Multithreaded Portable Runtime/web"
+#define BIT_CONFIG_CMD "bit -d -without all -configure ."
+#define BIT_PROFILE "debug"
+#define BIT_CFG_PREFIX "C:/Program Files/Multithreaded Portable Runtime"
+#define BIT_BIN_PREFIX "C:/Program Files/Multithreaded Portable Runtime/bin"
+#define BIT_INC_PREFIX "C:/Program Files/Multithreaded Portable Runtime/inc"
+#define BIT_LOG_PREFIX "C:/Program Files/Multithreaded Portable Runtime/logs"
+#define BIT_PRD_PREFIX "C:/Program Files/Multithreaded Portable Runtime"
+#define BIT_SPL_PREFIX "C:/Program Files/Multithreaded Portable Runtime/tmp"
+#define BIT_SRC_PREFIX "C:/Program Files/Multithreaded Portable Runtime/src"
+#define BIT_VER_PREFIX "C:/Program Files/Multithreaded Portable Runtime"
+#define BIT_WEB_PREFIX "C:/Program Files/Multithreaded Portable Runtime/web"
 #define BIT_EXE ".exe"
 #define BIT_SHLIB ".lib"
 #define BIT_SHOBJ ".dll"
@@ -54,11 +54,11 @@
 #define BIT_FEATURE_WINSDK 1
 #define BIT_FEATURE_CC 1
 #define BIT_FEATURE_LINK 1
-#define BIT_FEATURE_DUMPBIN 0
+#define BIT_FEATURE_DUMPBIN 1
 #define BIT_FEATURE_RC 1
 #define BIT_FEATURE_EJS 1
 #define BIT_FEATURE_HTTP 1
-#define BIT_FEATURE_MD5 1
+#define BIT_FEATURE_MD5 0
 #define BIT_FEATURE_MATRIXSSL 0
 #define BIT_FEATURE_OPENSSL 0
 #define BIT_FEATURE_UTEST 1
