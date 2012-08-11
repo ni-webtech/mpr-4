ca509be159c5cda908c7fb3b2412dafe2158e146
Embedthis Software <dev@embedthis.com>
2012-08-11 09:42:38 -0700

    Fix ssl context free

--- src/mprOpenssl.c
+++ src/mprOpenssl.c
@@ -256,8 +256,6 @@ static MprOpenSsl *createOpenSslConfig(MprSsl *ssl, int server)
         mprError("OpenSSL: Unable to create SSL context"); 
         return 0;
     }
-    ossl->context = context;
-
     SSL_CTX_set_app_data(context, (void*) ssl);
     SSL_CTX_sess_set_cache_size(context, 512);
     RAND_bytes(resume, sizeof(resume));
@@ -342,6 +340,7 @@ static MprOpenSsl *createOpenSslConfig(MprSsl *ssl, int server)
         Ensure we generate a new private key for each connection
      */
     SSL_CTX_set_options(context, SSL_OP_SINGLE_DH_USE);
+    ossl->context = context;
     return ossl;
 }
 
